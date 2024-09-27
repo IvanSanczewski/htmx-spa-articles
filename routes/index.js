db571dc91d31ee4237c134f630455f0d8b9f62b1
IvanSanczewski <isanczewski@gmail.com>
2024-09-27 17:11:30 +0300

    Applied hx-boost to form

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,31 @@ import articles from '../data/articles.js';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index', {title: 'Product Listing', articles: articles});
+  // res.render('index', {title: 'Product Listing', articles: articles});
+  res.render('index', {title: 'Product Listing', articles});
 });
 
 router.get('/articles/:id', (req, res) => {
   const article = articles.find((a) => a.id === parseInt(req.params.id));
 
-  res.render('article', {title:article.name, article: article})
+  // res.render('article', {title:article.name, article: article})
+  res.render('article', {title:article.name, article});
+});
+
+router.post('/articles', (req, res) => {
+  // const name = req.body.name;
+  // const body = req.body.body;
+  const { name, body } = req.body;
+
+  const article = {
+    // generate a random id that is multiplied by a massive nr just to get natural id nr. USE A METHOD THAT GENERATES UNIQUE ID NRs FOR PRODUCTION
+    id: Math.floor(Math.random() * 100000000),
+    name,
+    body
+  }
+  articles.push(article);
+
+  res.render('partials/list', {articles});
 });
 
 export default router;
